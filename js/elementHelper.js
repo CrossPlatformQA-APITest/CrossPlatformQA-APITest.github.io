542d8cf7c1d03a16c8e9db5e2d8bfb298bc37eec
Mikael Elofsson <mikael.elofsson@soprasteria.com>
2018-10-09 15:54:27 +0200

    Added webproject

---
+++ js/elementHelper.js
@@ -0,0 +1,137 @@
+
+
+
+var timer;
+function resetTimer() {
+        clearTimeout(timer);
+		timer = setTimeout(logOut, 3000)
+		$("#loggedinmessage").css('display','inline-block')
+        // 1000 milisec = 1 sec
+	}
+	
+function logOut() {
+	   $("#loggedinmessage").css('display','none')
+	   document.cookie="loggedin=false";
+	   document.removeEventListener("click", resetTimer)
+	}
+	
+function logIn(){
+	$("#loggedinmessage").css('display','inline-block')
+	timer = setTimeout(logOut,10000);
+	document.cookie="firstname=John";
+	alert(document.cookie)
+	document.addEventListener("click", resetTimer);
+	}
+
+function showAndEnableButton(){
+	$("#hidden-disabled-button").css("display","inline");
+	setTimeout(function(){
+		$("#hidden-disabled-button").removeAttr("disabled")
+	},500);
+}
+
+function hideElement(id){
+	$("#"+id).css("visibility","hidden");
+}
+
+function switchtotextinput(){
+	$("#inputelement").attr("type","text");
+}
+
+function showNestedElements(){
+	$('.child-ul').css('display','inline-block');
+}
+
+var backgroundStriped = false;
+
+function hideAfterTimeout(id, milliseconds){
+setTimeout(function(){hideElement(id);},milliseconds);
+}
+
+function showAfterTimeout(id, milliseconds){
+setTimeout(function(){showElement(id);},milliseconds);
+}
+
+function showAfterTimeoutByClass(className, milliseconds){
+	setTimeout(function(){showElementByClass(className);},milliseconds);
+	}
+
+function removeAfterTimeout(id, milliseconds){
+setTimeout(function(){removeElement(id);},milliseconds);	
+}
+function insertTextAfterTimeout(id, milliseconds,textToInsert){
+setTimeout(function(){insertText(id,textToInsert);},milliseconds);
+}
+
+function setStripedBackground(milliseconds){
+	setTimeout(function(){
+
+	if (backgroundStriped){
+		$("#striped-div").css("background-image", "");  
+		$("#striped-div").css("background-size", "");
+	} else {
+	$("#striped-div").css("background-image", "url(../img/stripes.jpg)");  
+	$("#striped-div").css("background-size", "contain");
+	}
+	backgroundStriped = !backgroundStriped;	
+	
+	},milliseconds);
+}
+
+function startRotation(){
+	$("#rotatingimage").addClass('imageanimation');
+}
+
+function insertText(id, textToInsert){
+insertText(id,textToInsert);
+}
+
+function showElement(id){
+	$("#"+id).css("display","inline-block");
+}
+function showElementByClass(className){
+	$("."+className).css("display","inline-block");
+}
+function removeElement(id){
+	$("#"+id).remove();
+}
+
+function changeBackgroundColorAfterTimeOut(id, color,timeout){
+	setTimeout(function(){changeBackgroundColor(id,color);},timeout);
+}
+
+
+function changeBackgroundColor(id,color){
+	$("#"+id).css("background-color",color);
+}
+
+function insertText(id, string){
+$("#"+id).html(string);
+}
+function removeButton() {
+    $("#delayedremovedelement").remove();
+}
+
+function showAfterCustomTimeOut(timeout){
+setTimeout(function(){hideElement(id);},milliseconds);
+}
+
+function appendButton(){
+   $("#elementsvisibility-second").append( "<button id='createdelement' type='button' class='btn btn-warning col-xs-6'>#createdelement</button><br>" );
+}
+
+function changeBackground(){
+	$("#headernav").css('background-color','red');
+	$("#page-wrapper").css('background-color','red');
+}
+
+function appendandhideButton(){
+$("#elementsvisibility").append( "<button id='createdhiddenbutton' type='button' class='btn btn-danger'>#createdhiddenbutton</button>" );
+$("#createdhiddenbutton").css("display","none");
+$("#createandhidebuttons").append( "Knappen har skapats men är inte synlig" );
+}
+
+
+
+
+
